be11a19620ef37ff2324dfe4fb3463b13cd88e45
ricarellano <rarellanob@gmail.com>
2020-09-13 19:03:48 -0700

    created cards

--- pages/_app.js
+++ pages/_app.js
@@ -16,6 +16,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>Rehash Take Home Challenge</title>
         <link rel="icon" href="/favicon.ico" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet"/>
       </Head>
       <header className={styles.header}>
         {/* MODIFY AS NEEDED */}
